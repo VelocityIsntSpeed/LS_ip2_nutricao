839a892d4937d0b0a869bc88edc15cde7bb633e8
samue <samue@DESKTOP-QQ6HNV5>
2021-06-11 19:49:12 -0300

    Comida e InfoNutricional

--- src/Comida.java
+++ src/Comida.java
@@ -1,4 +1,38 @@
 
 public class Comida {
+	private InfoNutricional infoNutricional;
+	private String nome;
+	private Comida[] ingredientes;
+	
+	
+	public Comida(InfoNutricional infoNutricional, String nome, Comida[] ingredientes) {
+		super();
+		this.infoNutricional = infoNutricional;
+		this.nome = nome;
+		this.ingredientes = ingredientes;
+	}
+	
+	
+	public InfoNutricional getInfoNutricional() {
+		return infoNutricional;
+	}
+	public void setInfoNutricional(InfoNutricional infoNutricional) {
+		this.infoNutricional = infoNutricional;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Comida[] getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(Comida[] ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+	
 	
 }
--- src/InfoNutricional.java
+++ src/InfoNutricional.java
@@ -1,4 +1,44 @@
 
 public class InfoNutricional {
-
+	private double valEnergetico;
+	private double proteina;
+	private double carboidratos;
+	private double etc;
+	
+	
+	
+	public InfoNutricional(double valEnergetico, double proteina, double carboidratos, double etc) {
+		super();
+		this.valEnergetico = valEnergetico;
+		this.proteina = proteina;
+		this.carboidratos = carboidratos;
+		this.etc = etc;
+	}
+	
+	public double getValEnergetico() {
+		return valEnergetico;
+	}
+	public void setValEnergetico(double valEnergetico) {
+		this.valEnergetico = valEnergetico;
+	}
+	public double getProteina() {
+		return proteina;
+	}
+	public void setProteina(double proteina) {
+		this.proteina = proteina;
+	}
+	public double getCarboidratos() {
+		return carboidratos;
+	}
+	public void setCarboidratos(double carboidratos) {
+		this.carboidratos = carboidratos;
+	}
+	public double getEtc() {
+		return etc;
+	}
+	public void setEtc(double etc) {
+		this.etc = etc;
+	}
+	
+	
 }
