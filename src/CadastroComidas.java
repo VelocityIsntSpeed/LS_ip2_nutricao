eb00808dd95bee41e16b24600df0faed09e89096
rksamu <rksamu@users.noreply.github.com>
2021-06-11 19:58:58 -0300

    CadastroComidas

--- src/CadastroComidas.java
+++ src/CadastroComidas.java
@@ -1,4 +1,19 @@
 
 public class CadastroComidas {
+	private Comida[] comidas;
+	
+	public CadastroComidas(Comida[] comidas) {
+		super();
+		this.comidas = comidas;
+	}
 
+	public Comida[] getComidas() {
+		return comidas;
+	}
+
+	public void setComidas(Comida[] comidas) {
+		this.comidas = comidas;
+	}
+	
+	
 }
