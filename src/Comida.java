a4a1ec0816f2ea2d0192f07651477159e6715708
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2021-06-19 04:27:45 -0300

    Remove estrutura de árvore de Comida (close #9)

--- src/Comida.java
+++ src/Comida.java
@@ -1,15 +1,12 @@
-import java.util.ArrayList;
 
 public class Comida {
 	private String nome;
 	private InfoNutricional infoNutricional;
-	private ArrayList<Comida> ingredientes;
 	
-	public Comida(String nome, InfoNutricional infoNutricional, ArrayList<Comida> ingredientes) {
+	public Comida(String nome, InfoNutricional infoNutricional) {
 		super();
 		this.nome = nome;
 		this.infoNutricional = infoNutricional;
-		this.ingredientes = ingredientes;
 	}
 
 	public String getNome() {
@@ -25,24 +22,9 @@ public class Comida {
 	public void setInfoNutricional(InfoNutricional infoNutricional) {
 		this.infoNutricional = infoNutricional;
 	}
-	
-	public ArrayList<Comida> getIngredientes() {
-		return ingredientes;
-	}
-	public void setIngredientes(ArrayList<Comida> ingredientes) {
-		this.ingredientes = ingredientes;
-	}
-	
-	private String possuiIngredientes() {
-		if (this.ingredientes != null) {
-			return "Sim";
-		}
-		return "Não";
-	}
-	
+
 	@Override
 	public String toString() {
-		return "Comida: " + nome + "/nPossui Ingredientes: " 
-				+ this.possuiIngredientes() + "/nInformação Nutricional: " + infoNutricional;
+		return String.format("Comida [nome=%s, infoNutricional=%s]", nome, infoNutricional);
 	}
 }
