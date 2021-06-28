3634d9db75f56e1c67c1cf2596b8a399f0048b71
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2021-06-28 04:29:00 -0300

    Funcionalidade da GUI de add comida (close #17)

--- src/tempPackage/TelaComidasController.java
+++ src/tempPackage/TelaComidasController.java
@@ -18,7 +18,20 @@ public class TelaComidasController {
 	@FXML private TextField txtFieldProteina;
 	@FXML private TextField txtFieldCarboidratos;
 	
+	// TODO Quire #23 #24
 	@FXML private void btnAddComida(ActionEvent event) {
-		System.out.println("Botão de adicionar comida foi clicado.");
+		
+		InfoNutricional infoNut = new InfoNutricional(Double.parseDouble(txtFieldValEnergetico.getText()),
+				                                      Double.parseDouble(txtFieldProteina.getText()),
+				                                      Double.parseDouble(txtFieldCarboidratos.getText()));
+		Comida comida = new Comida(txtFieldNome.getText(), infoNut);
+		
+		instanciaDoApp.repoComidas.add(comida);
+		
+		// Limpa os campos
+		txtFieldNome.clear();
+		txtFieldValEnergetico.clear();
+		txtFieldProteina.clear();
+		txtFieldCarboidratos.clear();
 	}
 }
