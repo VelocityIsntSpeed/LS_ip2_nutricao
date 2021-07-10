18d3ac82f0544ae11063e48293ace3143e2259b7
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2021-07-09 23:21:42 -0300

    Add persistencia de refeicoes

--- src/lsss/appNutri/negocios/RepositorioRefeicoes.java
+++ src/lsss/appNutri/negocios/RepositorioRefeicoes.java
@@ -1,12 +1,20 @@
 package lsss.appNutri.negocios;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import lsss.appNutri.persistencia.ListaDAO;
 
 public class RepositorioRefeicoes {
 	/** ObservableList pode ser usado como um ArrayList. A diferença é que é possível
 	 * setar observadores que são notificados quando há alguma mudança na lista. */
-	private final ObservableList<Refeicao> refeicoes = FXCollections.observableArrayList();
+	private final ObservableList<Refeicao> refeicoes;
+	private final ListaDAO dao = new ListaDAO("Refeicoes.txt");
+
+	// Construtor
+	@SuppressWarnings("unchecked")
+	public RepositorioRefeicoes() {
+		refeicoes = dao.carregar();
+	}
+	
 	
 	public ObservableList<Refeicao> getObservableList() {
 		return refeicoes;
@@ -17,13 +25,19 @@ public class RepositorioRefeicoes {
 		if (arg != null && !refeicoes.contains(arg)) {
 			refeicoes.add(arg);
 		}
+		
+		dao.salvar(refeicoes);
 	}
 	
 	/** Remove uma refeição.
 	 * @return true se a refeição foi removida, false se não foi removida
 	 * porque a refeição não estava contida no repositório. */
 	public boolean remover(Refeicao arg) {
-		return refeicoes.remove(arg);
+		boolean foiRemovida = refeicoes.remove(arg);
+		
+		dao.salvar(refeicoes);
+		
+		return foiRemovida;
 	}
 	
 	@Override
